b16e6dec6fee2ccd29e1ba096bb1fdcd61bc6f9b
akshatdubey <37729772+akshat7606@users.noreply.github.com>
2020-07-09 14:37:11 +0530

    Add files via upload

---
+++ islandPerimeter.java
@@ -0,0 +1,28 @@
+
+public class islandPerimeter {
+    public static int islandPerimeter(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    count += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        count -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        count -= 2;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 } };
+        System.out.println(islandPerimeter.islandPerimeter(grid));
+    }
+}
